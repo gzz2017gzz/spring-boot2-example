2710508ff0fd635314aeffbd696f00c502bb5970
gzz <gzz@163.com>
2020-03-13 22:23:16 +0800

    去除多于的包

--- 84-spring-boot-jdbc-poi/src/main/java/com/gzz/sys/table/Field.java
+++ 84-spring-boot-jdbc-poi/src/main/java/com/gzz/sys/table/Field.java
@@ -1,21 +1,55 @@
 package com.gzz.sys.table;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 /**
  * @author https://www.jianshu.com/u/3bd57d5f1074
  * @date 2019-12-24 14:50:00
  */
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class Field {
 	private String column_name;
 	private String column_comment;
 	private String column_type;
 	private String is_nullable;
 	private String pri;
+
+	public String getColumn_name() {
+		return column_name;
+	}
+
+	public void setColumn_name(String column_name) {
+		this.column_name = column_name;
+	}
+
+	public String getColumn_comment() {
+		return column_comment;
+	}
+
+	public void setColumn_comment(String column_comment) {
+		this.column_comment = column_comment;
+	}
+
+	public String getColumn_type() {
+		return column_type;
+	}
+
+	public void setColumn_type(String column_type) {
+		this.column_type = column_type;
+	}
+
+	public String getIs_nullable() {
+		return is_nullable;
+	}
+
+	public void setIs_nullable(String is_nullable) {
+		this.is_nullable = is_nullable;
+	}
+
+	public String getPri() {
+		return pri;
+	}
+
+	public void setPri(String pri) {
+		this.pri = pri;
+	}
+
 }
--- 84-spring-boot-jdbc-poi/src/main/java/com/gzz/sys/table/Table.java
+++ 84-spring-boot-jdbc-poi/src/main/java/com/gzz/sys/table/Table.java
@@ -1,18 +1,28 @@
 package com.gzz.sys.table;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 /**
  * @author https://www.jianshu.com/u/3bd57d5f1074
  * @date 2019-12-24 14:50:00
  */
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class Table {
 	private String table_name;
 	private String table_comment;
+
+	public String getTable_name() {
+		return table_name;
+	}
+
+	public void setTable_name(String table_name) {
+		this.table_name = table_name;
+	}
+
+	public String getTable_comment() {
+		return table_comment;
+	}
+
+	public void setTable_comment(String table_comment) {
+		this.table_comment = table_comment;
+	}
+
 }
